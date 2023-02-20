6f1a7fb6e517992077196e4b109aa340bd5d775e
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-20 15:09:11 -0300

    feat: added global container styled components

---
+++ portfolio/src/styles/Containers.tsx
@@ -0,0 +1,14 @@
+import { styled } from "./stitches.config";
+
+export const Section = styled("section", {
+  padding: "80px 10px",
+  width: "100%",
+  height: "100%",
+});
+
+export const Flex = styled("div", {
+  display: "flex",
+  width: "100%",
+  height: "100%",
+  flexWrap: "wrap",
+});
